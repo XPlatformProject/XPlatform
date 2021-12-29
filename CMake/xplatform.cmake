764d670fd1f3e065b73352c10b792c448aa3f987
0xHorror-Dev <horror-dev@bk.ru>
2021-12-29 18:48:40 +0300

    .dll file type fixes #2

--- CMake/xplatform.cmake
+++ CMake/xplatform.cmake
@@ -4,7 +4,7 @@ add_custom_command(
         TARGET ${target} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
                 $<TARGET_FILE:${dependence}>
-                $<TARGET_FILE_DIR:${target}>/${dependence})
+                $<TARGET_FILE_DIR:${target}>)
 
 add_dependencies(${target} ${dependence})
 endfunction(xplatform_add_dependence)
